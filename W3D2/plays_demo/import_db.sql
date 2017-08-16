e4226d458010329fb903eb0ced4ef2980549df77
John Schoeman <johnschoeman1617@gmail.com>
2017-08-16 08:26:36 -0700

    Complete W3D3 hw - Intro Rails

---
+++ W3D2/plays_demo/import_db.sql
@@ -0,0 +1,12 @@
+CREATE TABLE plays (
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  year INTEGER NOT NULL,
+  playwright_id INTEGER NOT NULL,
+
+  FOREIGN KEY (playwright_id) REFERENCES playwrights(id)
+);
+
+CREATE TABLE playwrights (
+
+);
