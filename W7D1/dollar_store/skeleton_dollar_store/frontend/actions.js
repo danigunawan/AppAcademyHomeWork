bfa902ee8c85ee8db5610d6dc4ab81b8f669893d
John Schoeman <johnschoeman1617@gmail.com>
2017-09-10 10:23:43 -0700

    Finish W7D1 hw - dollar store redux

---
+++ W7D1/dollar_store/skeleton_dollar_store/frontend/actions.js
@@ -0,0 +1,10 @@
+const SWITCH_CURRENCY = 'SWITCH_CURRENCY';
+
+const selectCurrency = (baseCurrency = "", rates = {}) => {
+  return {type: SWITCH_CURRENCY,
+  baseCurrency: baseCurrency,
+  rates: rates};
+};
+
+window.selectCurrency = selectCurrency;
+export default selectCurrency;
